fc8859f5f746badefc1817b9001c9bc6789a3547
noahmonks2 <noahmonks2@gmail.com>
2026-02-26 07:04:43 +0000

    File changes

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import EnrichedVisitsCalculator from '../components/pricing/EnrichedVisitsCalculator';
+// eslint-disable-next-line no-unused-vars
 
 const S = { muted: '#D9ECFF' };
 
