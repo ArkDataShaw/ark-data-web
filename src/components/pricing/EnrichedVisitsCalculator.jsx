b79cf7ce37b00bb5b128fe2131a9a85fc168e23b
noahmonks2 <noahmonks2@gmail.com>
2026-03-02 06:35:11 +0000

    File changes

--- src/components/pricing/EnrichedVisitsCalculator.jsx
+++ src/components/pricing/EnrichedVisitsCalculator.jsx
@@ -38,7 +38,7 @@ function fmtUSD(n) {
   return '$' + n.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 }
 
-const CHART_POINTS = [1000, 5000, 15000, 30000, 50000, 100000, 250000];
+const CHART_POINTS = [500, 2500, 10000, 25000, 50000, 100000, 200000];
 
 function buildChartData(ratePercent) {
   return CHART_POINTS.map(enriched => {
